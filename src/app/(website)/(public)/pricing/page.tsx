1f13d438e3f520be75e65a7dfc7f76abf31e2108
mubashirhussan <mubashirhussan93@gmail.com>
2025-06-10 09:33:58 +0500

    faq added below pricing

--- src/app/(website)/(public)/pricing/page.tsx
+++ src/app/(website)/(public)/pricing/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/container";
 import { PricingPlans } from "@/components/dashboard/pricing-plans";
+import FAQsTwo from "@/components/home4/home-faqs";
 import { PricingFaq } from "@/components/pricing/pricing-faq";
 import { HeaderSection } from "@/components/shared/header-section";
 import { siteConfig } from "@/config/site";
@@ -13,45 +14,46 @@ export const metadata = constructMetadata({
 
 export default async function PricingPage() {
   return (
-    <Container className="mt-8 pb-16">
-      <div className="w-full flex flex-col gap-16">
-        <section className="w-full flex flex-col gap-8 justify-center">
-          <HeaderSection
-            labelAs="h1"
-            label="Pricing"
-            titleAs="h2"
-            title="Choose a pricing plan"
-          />
+    // <Container className="mt-8 pb-16 max-w-6xl">
+    <div className="w-full mx-auto flex flex-col gap-16 mt-8 pb-16 max-w-6xl">
+      <section className="w-full flex flex-col gap-8 justify-center">
+        <HeaderSection
+          labelAs="h1"
+          label="Pricing"
+          titleAs="h2"
+          title="Choose a pricing plan"
+        />
 
-          <div className="w-full mx-auto">
-            <PricingPlans />
-          </div>
+        <div className="w-full mx-auto">
+          <PricingPlans />
+        </div>
 
-          {/* add tips only for Mkdirs demo directory website */}
-          {siteConfig.name === "Directory" && (
-            <p className="text-center text-sm text-muted-foreground leading-normal">
-              This is in <span className="font-bold">TEST</span> environment.
-              <br />
-              You can use Stripe test card to simulate the paid submission
-              process.
-              <br />
-              Stripe test card number: 4242 4242 4242 4242
-            </p>
-          )}
-        </section>
+        {/* add tips only for Mkdirs demo directory website */}
+        {siteConfig.name === "Directory" && (
+          <p className="text-center text-sm text-muted-foreground leading-normal">
+            This is in <span className="font-bold">TEST</span> environment.
+            <br />
+            You can use Stripe test card to simulate the paid submission
+            process.
+            <br />
+            Stripe test card number: 4242 4242 4242 4242
+          </p>
+        )}
+      </section>
 
-        <section className="w-full flex flex-col gap-8 justify-center">
-          <HeaderSection
-            label="FAQ"
-            titleAs="h2"
-            title="Frequently Asked Questions"
-          />
+      <section className="w-full flex flex-col gap-8 justify-center">
+        <HeaderSection
+          label="FAQ"
+          titleAs="h2"
+          title="Frequently Asked Questions"
+        />
 
-          <div className="w-full max-w-4xl mx-auto">
-            <PricingFaq />
-          </div>
-        </section>
-      </div>
-    </Container>
+        <div className="w-full max-w-4xl mx-auto">
+          <FAQsTwo />
+          {/* <PricingFaq /> */}
+        </div>
+      </section>
+    </div>
+    // </Container>
   );
 }

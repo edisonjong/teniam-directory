90e02266298b58eb412bc57b80cc0081799f48d8
mubashirhussan <mubashirhussan93@gmail.com>
2025-07-10 15:39:38 +0500

    added blog newsletter

--- src/app/(website)/(public)/blog/[slug]/layout.tsx
+++ src/app/(website)/(public)/blog/[slug]/layout.tsx
@@ -1,17 +1,20 @@
 import Container from "@/components/container";
 import { NewsletterCard } from "@/components/newsletter/newsletter-card";
 import type React from "react";
+import SubscribeSection from "../../sections/subscribe-section";
 
 export default function BlogPostLayout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="mb-16">
       <Container className="mt-8">{children}</Container>
 
-      <Container className="mt-16">
-        <NewsletterCard />
-      </Container>
+      <div className="mt-16">
+        <SubscribeSection />
+      </div>
     </div>
   );
 }

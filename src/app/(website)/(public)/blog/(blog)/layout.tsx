3a2a5dcca9a30ad25f6364c67fc492ea2844311e
mubashirhussan <mubashirhussan93@gmail.com>
2025-07-10 14:46:14 +0500

    added blog newsletter

--- src/app/(website)/(public)/blog/(blog)/layout.tsx
+++ src/app/(website)/(public)/blog/(blog)/layout.tsx
@@ -1,29 +1,33 @@
 import { BlogCategoryFilter } from "@/components/blog/blog-category-filter";
+import BlogNewsLetter from "@/components/blog/blog-newslwtter";
 import Container from "@/components/container";
 import { NewsletterCard } from "@/components/newsletter/newsletter-card";
 import { HeaderSection } from "@/components/shared/header-section";
 
 export default async function BlogListLayout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="mb-16">
-      <div className="mt-8 w-full flex flex-col items-center justify-center gap-8">
-        <HeaderSection
+      <div className=" w-full flex flex-col items-center justify-center gap-8">
+        <BlogNewsLetter />
+        {/* <HeaderSection
           labelAs="h1"
           label="Blog"
           titleAs="h2"
           title="Read our latest blog posts"
-        />
+        /> */}
 
         <BlogCategoryFilter />
       </div>
 
       <Container className="mt-8">{children}</Container>
 
-      <Container className="mt-8">
+      {/* <Container className="mt-8">
         <NewsletterCard />
-      </Container>
+      </Container> */}
     </div>
   );
 }

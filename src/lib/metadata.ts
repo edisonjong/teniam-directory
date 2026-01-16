9e0823ecf38287d22264090df0259735951d232e
cybertycoon <silasokanla2006@gmail.com>
2026-01-16 12:47:21 +0100

    apple touch icon

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -89,7 +89,7 @@ export function constructMetadata({
     icons: {
       icon: "/favicon.ico",
       shortcut: "/favicon-32x32.png",
-      apple: "/apple-touch-icon.png",
+      apple: "/favicon.ico.jpeg",
     },
     metadataBase: new URL(siteConfig.url),
     manifest: `${siteConfig.url}/site.webmanifest`,
